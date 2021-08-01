7c134000371692e54c94bdb965a36a757e470351
Sergio Franco <sergiofranco6@gmail.com>
2021-08-01 17:56:07 -0400

    added exception handling system

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,22 +18,17 @@ public class Main {
 
 	 */
 
-//        int[] myArray = {1, 2, 6, 3};
-//        int[] myArray2 = new int[2];
-//
-//        myArray2[0] = 3;
-
         Scanner scanner = new Scanner(System.in);
 
         Cup cup = new Cup();
         cup.roll();
 
-        int[] bidArray = new int[4];
+        int[] bidArray = new int[2];
 
-        int numberOfDice = 0;
-        int faceUpValue = 0;
-        int numberOfDice2 = 0;
-        int faceUpValue2 = 0;
+        int numberOfDice;
+        int faceUpValue;
+        int numberOfDice2;
+        int faceUpValue2;
 
 
         System.out.println("Hand: " + cup.displayCup());
@@ -46,16 +41,19 @@ public class Main {
 
         System.out.println("Current bid: \nDice: " + bidArray[0] + " Value: " + bidArray[1]);
 
-        System.out.print("Please enter second bid\nNumber of dice: ");
-        numberOfDice2 = scanner.nextInt();
-        bidArray[2] = numberOfDice2;
-        System.out.print("Second face up value of dice: ");
-        faceUpValue2 = scanner.nextInt();
-        bidArray[3] = faceUpValue2;
-        if (numberOfDice2 < numberOfDice || faceUpValue2 < faceUpValue) {
-            System.out.println("Please enter valid bid");
-        } else {
-            System.out.println("Current bid: \nDice: " + bidArray[2] + " Value: " + bidArray[3]);
+        while (true) {
+            System.out.print("Please enter second bid\nNumber of dice: ");
+            numberOfDice2 = scanner.nextInt();
+            bidArray[0] = numberOfDice2;
+            System.out.print("Second face up value of dice: ");
+            faceUpValue2 = scanner.nextInt();
+            bidArray[1] = faceUpValue2;
+            if (numberOfDice2 < numberOfDice || faceUpValue2 < faceUpValue) {
+                System.out.println("Please enter valid bid");
+            } else {
+                System.out.println("Current bid: \nDice: " + bidArray[0] + " Value: " + bidArray[1]);
+                break;
+            }
         }
     }
 
