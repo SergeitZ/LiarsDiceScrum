6c2e67f1962cfcf553bff706edd8188ac6d09dd9
SergeitZ <sergiofranco6@gmail.com>
2021-08-05 20:48:47 -0400

    Added active player system

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -7,26 +7,45 @@ public class Game {
     Cup cup = new Cup();
     public Player player;
     public List<Player> players = new ArrayList<>();
+    int [] bidArray = new int[2];
+    Scanner scanner = new Scanner(System.in);
+    int MAX_DICE_ALLOWED = cup.dice.size();
     private final int MAX_PLAYERS = 6;
     private final int MIN_PLAYERS = 1;
 
 
     public Game () {
-//        System.out.print("Enter number of players: ");
-//        int numberOfPlayers;
-//        do {
-//            numberOfPlayers = scanner.nextInt();
-//            scanner.nextLine();
-//        } while (players.size() < MIN_PLAYERS || players.size() > MAX_PLAYERS);
-//
-//        while (players.size() < numberOfPlayers) {
-//            System.out.print("Enter name: ");
-//            players.add(new Player(scanner.nextLine().trim()));
-//        }
+        System.out.print("Enter number of players: ");
+        int numberOfPlayers;
+        do {
+            numberOfPlayers = scanner.nextInt();
+            scanner.nextLine();
+        } while (numberOfPlayers < MIN_PLAYERS || numberOfPlayers > MAX_PLAYERS);
+
+        while (players.size() < numberOfPlayers) {
+            System.out.print("Enter name: ");
+            players.add(new Player(scanner.nextLine().trim()));
+        }
+    }
+
+    public void play() {
+        round();
+    }
+
+    //TODO: implement challenge previous bid system
+    public void turn(Player activePlayer) {
+        System.out.println("\n" + activePlayer.name + "'s turn.");
+        activePlayer.cup.roll();
+        makeBid(activePlayer);
+    }
+
+    public void round() {
+        for (Player activePlayer : players) {
+            turn(activePlayer);
+        }
     }
 
     public int getValidBid(String prompt, int min, int max) {
-        Scanner scanner = new Scanner(System.in);
         int value;
         while (true) {
             System.out.print(prompt);
@@ -38,12 +57,22 @@ public class Game {
         return value;
     }
 
-    public void makeBid() {
-        cup.roll();
-        int MAX_DICE_ALLOWED = cup.dice.size();
-        int [] bidArray = new int[2];
-        System.out.println("Hand: " + cup.displayCup());
+
+    public void makeBid(Player activePlayer) {
+
+        System.out.println("Hand: " + activePlayer.cup.displayCup());
         bidArray[0] = getValidBid("Enter dice amount: ", 1, MAX_DICE_ALLOWED);
         bidArray[1] = getValidBid("Enter face value: ", 1, 6);
+        System.out.println("\nCurrent bid:\nDice:" + bidArray[0] + "\nValue:" + bidArray[1]);
+
     }
+
+
+    public void followUpBid(Player activePlayer) {
+        System.out.println("\nFollow up bid");
+        bidArray[0] = getValidBid("Enter dice amount: ", bidArray[0], MAX_DICE_ALLOWED);
+        bidArray[1] = getValidBid("Enter face value: ", bidArray[1], MAX_DICE_ALLOWED);
+        System.out.println("\nCurrent bid:\nDice:" + bidArray[0] + "\nValue:" + bidArray[1]);
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	/*
-	1) single player can roll dice,
-        look at dice
-
-     2) make a bid, make a second valid bid (increase die value, or increase number of dice of any value),
-        ask for another bid if invalid bid given
-
-            * Create a variable to store bid
-            * Store a second bid
-
-     3) Organize code into classes
-            * Refactor methods (shorter)
-                - Create methods in game class that do a specific function
-
-
-     4) [Liar System]
-
-	 */
 
         Game game = new Game();
-        game.makeBid();
+        game.play();
 
     }
 }
